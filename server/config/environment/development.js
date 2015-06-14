da88a469643c449d13849f16b135939366bac357
Blair Morris <Blair.Morris@partner.bmwgroup.com>
2015-06-14 19:36:37 -0400

    Initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://eblairmorris:purpscurp@ds041190.mongolab.com:41190/mongomain'
+  },
+
+  seedDB: true
+};
