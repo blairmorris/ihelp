78a56042f7415a88b42f0236c552687e42c8e44c
Blair Morris <Blair.Morris@partner.bmwgroup.com>
2015-06-14 23:14:25 -0400

    Reset the dev and production stuff that I should not have changed in the first place.

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://eblairmorris:purpscurp@ds041190.mongolab.com:41190/mongomain'
+    uri: 'mongodb://localhost/ihelp-dev'
   },
 
   seedDB: true
